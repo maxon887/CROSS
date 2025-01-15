063ac6beef0ff85b5e7c3fea2ae758b1195ef825
Maxon <maxon887@gmail.com>
2025-01-15 15:43:04 +0300

    Imgui update

--- Demo/Sources/Demo.cpp
+++ Demo/Sources/Demo.cpp
@@ -32,6 +32,219 @@ using namespace cross;
 
 Demo* demo = nullptr;
 
+ImGuiKey FromCrossKeyToImgui(Key key) {
+	switch(key)	{
+	case cross::Key::BACKSPACE:
+		return ImGuiKey_Backspace;
+	case cross::Key::TAB:
+		return ImGuiKey_Tab;
+	case cross::Key::COMMAND:
+		return ImGuiMod_Ctrl;
+	case cross::Key::OPTION:
+		return ImGuiMod_Alt;
+	case cross::Key::CLEAR:
+		return ImGuiKey_NamedKey_BEGIN;//does not know this key
+	case cross::Key::ENTER:
+		return ImGuiKey_Enter;
+	case cross::Key::SHIFT:
+		return ImGuiMod_Shift;
+	case cross::Key::CONTROL:
+		return ImGuiMod_Ctrl;
+	case cross::Key::ALT:
+		return ImGuiMod_Alt;
+	case cross::Key::PAUSE:
+		return ImGuiKey_Pause;
+	case cross::Key::CAPSLOCK:
+		return ImGuiKey_CapsLock;
+	case cross::Key::ESCAPE:
+		return ImGuiKey_Escape;
+	case cross::Key::SPACE:
+		return ImGuiKey_Space;
+	case cross::Key::PAGE_UP:
+		return ImGuiKey_PageUp;
+	case cross::Key::PAGE_DOWN:
+		return ImGuiKey_PageDown;
+	case cross::Key::END:
+		return ImGuiKey_End;
+	case cross::Key::HOME:
+		return ImGuiKey_Home;
+	case cross::Key::LEFT:
+		return ImGuiKey_LeftArrow;
+	case cross::Key::UP:
+		return ImGuiKey_UpArrow;
+	case cross::Key::RIGHT:
+		return ImGuiKey_RightArrow;
+	case cross::Key::DOWN:
+		return ImGuiKey_DownArrow;
+	case cross::Key::INSERT:
+		return ImGuiKey_Insert;
+	case cross::Key::DEL:
+		return ImGuiKey_Delete;
+	case cross::Key::HELP:
+		return ImGuiKey_NamedKey_BEGIN;	//does not know this key
+	case cross::Key::NUM_0:
+		return ImGuiKey_0;
+	case cross::Key::NUM_1:
+		return ImGuiKey_1;
+	case cross::Key::NUM_2:
+		return ImGuiKey_2;
+	case cross::Key::NUM_3:
+		return ImGuiKey_3;
+	case cross::Key::NUM_4:
+		return ImGuiKey_4;
+	case cross::Key::NUM_5:
+		return ImGuiKey_5;
+	case cross::Key::NUM_6:
+		return ImGuiKey_6;
+	case cross::Key::NUM_7:
+		return ImGuiKey_7;
+	case cross::Key::NUM_8:
+		return ImGuiKey_8;
+	case cross::Key::NUM_9:
+		return ImGuiKey_9;
+	case cross::Key::A:
+		return ImGuiKey_A;
+	case cross::Key::B:
+		return ImGuiKey_B;
+	case cross::Key::C:
+		return ImGuiKey_C;
+	case cross::Key::D:
+		return ImGuiKey_D;
+	case cross::Key::E:
+		return ImGuiKey_E;
+	case cross::Key::F:
+		return ImGuiKey_F;
+	case cross::Key::G:
+		return ImGuiKey_G;
+	case cross::Key::H:
+		return ImGuiKey_H;
+	case cross::Key::I:
+		return ImGuiKey_I;
+	case cross::Key::J:
+		return ImGuiKey_J;
+	case cross::Key::K:
+		return ImGuiKey_K;
+	case cross::Key::L:
+		return ImGuiKey_L;
+	case cross::Key::M:
+		return ImGuiKey_M;
+	case cross::Key::N:
+		return ImGuiKey_N;
+	case cross::Key::O:
+		return ImGuiKey_O;
+	case cross::Key::P:
+		return ImGuiKey_P;
+	case cross::Key::Q:
+		return ImGuiKey_Q;
+	case cross::Key::R:
+		return ImGuiKey_R;
+	case cross::Key::S:
+		return ImGuiKey_S;
+	case cross::Key::T:
+		return ImGuiKey_T;
+	case cross::Key::U:
+		return ImGuiKey_U;
+	case cross::Key::V:
+		return ImGuiKey_V;
+	case cross::Key::W:
+		return ImGuiKey_W;
+	case cross::Key::X:
+		return ImGuiKey_X;
+	case cross::Key::Y:
+		return ImGuiKey_Y;
+	case cross::Key::Z:
+		return ImGuiKey_Z;
+	case cross::Key::NUMPAD_0:
+		return ImGuiKey_Keypad0;
+	case cross::Key::NUMPAD_1:
+		return ImGuiKey_Keypad1;
+	case cross::Key::NUMPAD_2:
+		return ImGuiKey_Keypad2;
+	case cross::Key::NUMPAD_3:
+		return ImGuiKey_Keypad3;
+	case cross::Key::NUMPAD_4:
+		return ImGuiKey_Keypad4;
+	case cross::Key::NUMPAD_5:
+		return ImGuiKey_Keypad5;
+	case cross::Key::NUMPAD_6:
+		return ImGuiKey_Keypad6;
+	case cross::Key::NUMPAD_7:
+		return ImGuiKey_Keypad7;
+	case cross::Key::NUMPAD_8:
+		return ImGuiKey_Keypad8;
+	case cross::Key::NUMPAD_9:
+		return ImGuiKey_Keypad9;
+	case cross::Key::MULTIPLY:
+		return ImGuiKey_KeypadMultiply;
+	case cross::Key::ADD:
+		return ImGuiKey_KeypadAdd;
+	case cross::Key::SEPARATOR:
+		return ImGuiKey_NamedKey_BEGIN;	//does not know this key
+	case cross::Key::SUBTRACT:
+		return ImGuiKey_KeypadSubtract;
+	case cross::Key::DECIMAL:
+		return ImGuiKey_KeypadDecimal;
+	case cross::Key::DIVIDE:
+		return ImGuiKey_KeypadDivide;
+	case cross::Key::F1:
+		return ImGuiKey_F1;
+	case cross::Key::F2:
+		return ImGuiKey_F2;
+	case cross::Key::F3:
+		return ImGuiKey_F3;
+	case cross::Key::F4:
+		return ImGuiKey_F4;
+	case cross::Key::F5:
+		return ImGuiKey_F5;
+	case cross::Key::F6:
+		return ImGuiKey_F6;
+	case cross::Key::F7:
+		return ImGuiKey_F7;
+	case cross::Key::F8:
+		return ImGuiKey_F8;
+	case cross::Key::F9:
+		return ImGuiKey_F9;
+	case cross::Key::F10:
+		return ImGuiKey_F10;
+	case cross::Key::F11:
+		return ImGuiKey_F11;
+	case cross::Key::F12:
+		return ImGuiKey_F12;
+	case cross::Key::NUMLOC:
+		return ImGuiKey_NumLock;
+	case cross::Key::SCROLLLOCK:
+		return ImGuiKey_ScrollLock;
+	case cross::Key::SEMICOLON:
+		return ImGuiKey_Semicolon;
+	case cross::Key::PLUS:
+		return ImGuiKey_Equal;
+	case cross::Key::COMMA:
+		return ImGuiKey_Comma;
+	case cross::Key::MINUS:
+		return ImGuiKey_Minus;
+	case cross::Key::DOT:
+		return ImGuiKey_Period;
+	case cross::Key::SLASH:
+		return ImGuiKey_Slash;
+	case cross::Key::TILDE:
+		return ImGuiKey_GraveAccent;
+	case cross::Key::L_SQUARE_BRACKET:
+		return ImGuiKey_LeftBracket;
+	case cross::Key::BACKSLASH:
+		return ImGuiKey_Backslash;
+	case cross::Key::R_SQUARE_BRACKET:
+		return ImGuiKey_RightBracket;
+	case cross::Key::APOSTROPHE:
+		return ImGuiKey_Apostrophe;
+	case cross::Key::BACK:
+		return ImGuiKey_NamedKey_BEGIN;	//does not know this key
+	case cross::Key::MAX_KEY_NUM:
+		return ImGuiKey_NamedKey_COUNT;
+	default:
+		break;
+	}
+}
+
 String Demo::GetCompactSize(U64 bytes) {
 	if(bytes < 10 * 1024) {
 		return String(bytes) + " b";
@@ -69,25 +282,26 @@ void Demo::Start() {
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO();
 	io.IniFilename = imgui_filename;
+	//io.SetKeyEventNativeData(ImGuiKey_Backspace, (int)Key::BACKSPACE, (int)Key::BACKSPACE);
 	// Keyboard mapping. ImGui will use those indices to peek into the io.KeyDown[] array.
-	io.KeyMap[ImGuiKey_Tab] = (int)Key::TAB;
-	io.KeyMap[ImGuiKey_LeftArrow] = (int)Key::LEFT;
-	io.KeyMap[ImGuiKey_RightArrow] = (int)Key::RIGHT;
-	io.KeyMap[ImGuiKey_UpArrow] = (int)Key::UP;
-	io.KeyMap[ImGuiKey_DownArrow] = (int)Key::DOWN;
-	io.KeyMap[ImGuiKey_PageUp] = (int)Key::PAGE_UP;
-	io.KeyMap[ImGuiKey_PageDown] = (int)Key::PAGE_DOWN;
-	io.KeyMap[ImGuiKey_Home] = (int)Key::HOME;
-	io.KeyMap[ImGuiKey_End] = (int)Key::END;
-	io.KeyMap[ImGuiKey_Delete] = (int)Key::DEL;
-	io.KeyMap[ImGuiKey_Backspace] = (int)Key::BACKSPACE;
-	io.KeyMap[ImGuiKey_Escape] = (int)Key::ESCAPE;
-	io.KeyMap[ImGuiKey_A] = (int)Key::A;
-	io.KeyMap[ImGuiKey_C] = (int)Key::C;
-	io.KeyMap[ImGuiKey_V] = (int)Key::V;
-	io.KeyMap[ImGuiKey_X] = (int)Key::X;
-	io.KeyMap[ImGuiKey_Y] = (int)Key::Y;
-	io.KeyMap[ImGuiKey_Z] = (int)Key::Z;
+	//io.KeyMap[ImGuiKey_Tab] = (int)Key::TAB;
+	//io.KeyMap[ImGuiKey_LeftArrow] = (int)Key::LEFT;
+	//io.KeyMap[ImGuiKey_RightArrow] = (int)Key::RIGHT;
+	//io.KeyMap[ImGuiKey_UpArrow] = (int)Key::UP;
+	//io.KeyMap[ImGuiKey_DownArrow] = (int)Key::DOWN;
+	//io.KeyMap[ImGuiKey_PageUp] = (int)Key::PAGE_UP;
+	//io.KeyMap[ImGuiKey_PageDown] = (int)Key::PAGE_DOWN;
+	//io.KeyMap[ImGuiKey_Home] = (int)Key::HOME;
+	//io.KeyMap[ImGuiKey_End] = (int)Key::END;
+	//io.KeyMap[ImGuiKey_Delete] = (int)Key::DEL;
+	//io.KeyMap[ImGuiKey_Backspace] = (int)Key::BACKSPACE;
+	//io.KeyMap[ImGuiKey_Escape] = (int)Key::ESCAPE;
+	//io.KeyMap[ImGuiKey_A] = (int)Key::A;
+	//io.KeyMap[ImGuiKey_C] = (int)Key::C;
+	//io.KeyMap[ImGuiKey_V] = (int)Key::V;
+	//io.KeyMap[ImGuiKey_X] = (int)Key::X;
+	//io.KeyMap[ImGuiKey_Y] = (int)Key::Y;
+	//io.KeyMap[ImGuiKey_Z] = (int)Key::Z;
 
 	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 
@@ -247,7 +461,7 @@ bool Demo::CreateFontsTexture() {
 		Texture::Compression::NONE,
 		Texture::TilingMode::CLAMP_TO_EDGE, false);
 	// Store our identifier
-	io.Fonts->TexID = (void *)(intptr_t)font_texture->GetID();
+	io.Fonts->TexID = (ImTextureID)font_texture->GetID();
 	return true;
 }
 
@@ -334,7 +548,7 @@ void Demo::ActionUpHandle(Input::Action action) {
 
 void Demo::KeyPressed(Key key) {
 	ImGuiIO& io = ImGui::GetIO();
-	io.KeysDown[(int)key] = true;
+	io.AddKeyEvent(FromCrossKeyToImgui(key), true);
 	if(key == Key::CONTROL) {
 		io.KeyCtrl = true;
 	}
@@ -351,7 +565,7 @@ void Demo::KeyPressed(Key key) {
 
 void Demo::KeyReleased(Key key) {
 	ImGuiIO& io = ImGui::GetIO();
-	io.KeysDown[(int)key] = false;
+	io.AddKeyEvent(FromCrossKeyToImgui(key), false);
 	if(key == Key::CONTROL) {
 		io.KeyCtrl = false;
 	}
